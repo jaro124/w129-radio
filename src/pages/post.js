0aa9d90bdbbabb2b9249794d22dc208f73385f0d
jaro124 <jaro124@gmail.com>
2024-05-13 14:05:11 +0200

    Adding grid list

--- src/pages/post.js
+++ src/pages/post.js
@@ -8,7 +8,20 @@ const PostPage = () => {
       <div className="px-4">
         
         
- 
+        
+        <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-2 xl:grid-cols-3 justify-end dark:hover:border-gray-500">
+          <div>01</div>
+          <div>02</div>
+          <div>03</div>
+          <div>04</div>
+          <div>05</div>
+        </div>
+        
+        <div className="ml-2">
+                          <span className="inline-flex items-center justify-center px-2 py-1 text-xs font-semibold leading-none text-black bg-gray-800 rounded-full">
+                            gatsbydddfsds
+                          </span>
+                        </div>
 
 
       </div>
--- src/templates/page-list-blog-template.js
+++ src/templates/page-list-blog-template.js
@@ -42,32 +42,41 @@ const PostListTemplate = ({
   return (
     <Layout>
       <div className="px-4">
-        <div className="max-w-4xl bg-white dark:bg-black rounded-lg mx-auto my-8 p-16">
-          <div class="text-center">
-            <h1 class="text-2xl font-semibold text-gray-800 capitalize lg:text-3xl dark:text-white">
+        <div className="max-w-4xl bg-white dark:bg-black rounded-lg mx-auto my-8 p-8">
+          <div className="text-center">
+            <h1 className="text-2xl font-semibold text-gray-800 capitalize lg:text-3xl dark:text-white">
               Radia
             </h1>
-
-            <p class="max-w-lg mx-auto mt-4 text-gray-500">
+            <p className="max-w-lg mx-auto mt-4 text-gray-500">
               Lorem ipsum dolor sit amet consectetur adipisicing elit.
             </p>
           </div>
-          <div class="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-2 xl:grid-cols-3">
+          <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 md:grid-cols-2 xl:grid-cols-3">
             {edges.map((edge) => (
               <Link to={"/radia/" + edge.node.frontmatter.slug}>
-                <div class="w-full max-w-xs overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800">
+                <div className="w-full max-w-xs overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 hover:shadow-2xl border dark:border-gray-800 hover:border-gray-200 dark:hover:border-gray-500">
                   <GatsbyImage
                     image={getImage(edge.node.frontmatter.hero_image)}
                     alt={edge.node.frontmatter.hero_image_alt}
                   />
 
-                  <div class="py-5 text-center">
-                    <span class="block text-xl font-bold text-gray-800 dark:text-white">
+                  <div className="py-2 text-center">
+                    <span className="block text-xl font-bold text-gray-800 dark:text-white">
                       {edge.node.frontmatter.title}
                     </span>
-                    <span class="text-sm text-gray-700 dark:text-gray-200">
+                    <span className="text-sm text-gray-700 dark:text-blue-400">
                       {edge.node.frontmatter.subtitle}
                     </span>
+                    <div className="bg-white dark:bg-gray-800 pt-2 mr-2 flex justify-end justify-items-end flex-wrap">
+                      {edge.node.frontmatter.tags.map((tag, index) => (
+                        <div className="ml-2">
+                          <span className="inline-flex items-center justify-center px-2 py-1 text-xs font-semibold leading-none text-black bg-orange-400 rounded-full">
+                            {tag}
+                          </span>
+                        </div>
+                        
+                      ))}
+                    </div>
                   </div>
                 </div>
               </Link>
